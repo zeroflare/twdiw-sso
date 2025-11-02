eb35e5d719c8ec0b6e69a655469b2353bb4c45dc
周詳 <hchou@moda.gov.tw>
2025-11-03 00:27:02 +0800

    Update oidc

--- src/controllers/oidc.controller.ts
+++ src/controllers/oidc.controller.ts
@@ -6,7 +6,7 @@ export class OIDCController {
     const { env, req } = c;
 
     const formData = await req.formData();
-    console.log(formData);
+    console.log(Object.fromEntries(formData));
     const code = formData.get("code");
 
     const raw = await c.env.CODE_KV.get(code as string);
